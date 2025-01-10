cc22d936e04e12a90fb0001fe090658b79ef981b
asarogluahmethakan <asarogluahmethakan@gmail.com>
2025-01-11 01:52:40 +0300

    Initial Commit

---
+++ HaritalarUygulamasi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HaritalarUygulamasi
+//
+//  Created by Ahmet Hakan Asaroğlu on 11.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HaritalarUygulamasiTests/HaritalarUygulamasiTests.swift
@@ -0,0 +1,17 @@
+//
+//  HaritalarUygulamasiTests.swift
+//  HaritalarUygulamasiTests
+//
+//  Created by Ahmet Hakan Asaroğlu on 11.01.2025.
+//
+
+import Testing
+@testable import HaritalarUygulamasi
+
+struct HaritalarUygulamasiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
